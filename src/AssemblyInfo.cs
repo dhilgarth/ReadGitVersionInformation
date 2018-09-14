92244d9a67a4f504f8c06ff9dbf2c289c84f2114
Daniel Hilgarth <d.hilgarth@fire-development.com>
2018-09-14 11:08:29 +0200

    Fix assembly info

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("fire-development Ltd.")]
+[assembly: AssemblyCopyright("Copyright © fire-development Ltd. 2018")]
+[assembly: AssemblyTitle("ReadGitVersionInformation")]
+[assembly: AssemblyProduct("ReadGitVersionInformation")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("Developer Build")]
